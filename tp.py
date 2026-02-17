33eb86e063caaf0258084dda638f460e0b1b09f5
dominguel <lucas.domingue@gmail.com>
2026-02-17 09:24:09 -0500

    second commit

--- tp.py
+++ tp.py
@@ -1,7 +1,6 @@
 def palette(x, y, couleurs):
     pass# undefined
-    # La procédure palette prend en paramètre deux entiers x et y et une liste de couleurs en format RGB444. Elle affiche cette palette de couleur sur une ligne horizontale commencant à la position (x,y).
-    # i.e. impure function; outputs to display
+    # impure function; outputs to display
 
     # proper type signature
     assert type(x) is int
@@ -16,7 +15,6 @@ def palette(x, y, couleurs):
 
 def afficher(x, y, forme, couleurs):
     pass# undefined
-    #  La procédure afficher prend en entrée deux entiers x et y, une forme (une matrice 2D contenant des index vers une couleur de la palette de couleur) et une palette de couleurs (une liste de couleurs au format RGB444). Elle affiche la forme à l'écran en utilisant les couleurs correspondantes. La valeur -1 dans la forme correspond à une case vide pour laquelle la couleur déjà présente dans le dessin doit être conservée.
 
     # proper type signature
     assert type(x) is int
@@ -31,14 +29,13 @@ def afficher(x, y, forme, couleurs):
     #TODO: verifier si les axes sont bien definis
     # voir la convention des axes; y, j := rangees
     for j in range(len(forme)):
-        # x,i := colonnes
+        # x, i := colonnes
         for i,c in enumerate(forme[j]):
             if c != -1:# si non-transparent
                 setPixel(x+i,y+j,couleurs[c])
 
 def effacer(x, y, largeur, hauteur):
     pass# undefined
-    #  La procédure effacer prend en entrée quatres entiers. Les deux premiers paramètres, x et y représentent la position du coin supérieur gauche d'un rectangle. Les dimensions du rectangle sont données par les deux derniers paramètres, largeur et hauteur. La procédure doit remplacer tous les pixels du rectangle par des pixels noirs ("#000").
 
     #TODO: type signature, tests, etc...
 
@@ -46,4 +43,3 @@ def effacer(x, y, largeur, hauteur):
 
 def superpose(forme1, forme2, x, y):
     pass# undefined
-    #  La fonction superpose prend deux formes (des matrices 2D) et retourne une nouvelle forme (une autre matrice 2D) ayant les mêmes dimensions de la forme1, mais à laquelle la forme2 a été ajoutée à la position (x, y). La forme2 est superposée à la forme1 de telle sorte que les pixels de la forme2 remplacent ceux de la forme1 à la position correspondante. Si un pixel de la forme2 est égal à -1, le pixel de la forme1 est conservé.
