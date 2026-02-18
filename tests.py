9fd7b3d0d6c50181cad877b94b2c08bcc7479b0d
dominguel <lucas.domingue@gmail.com>
2026-02-18 16:07:32 -0500

    assertion format improved

--- tests.py
+++ tests.py
@@ -1,120 +1,106 @@
-# La procédure palette prend en paramètre deux entiers x et y et une liste de
+# La procedure palette prend en parametre deux entiers x et y et une liste de
 # couleurs en format RGB444.
-# Elle affiche cette palette de couleur sur une ligne horizontale commencant à
+# Elle affiche cette palette de couleur sur une ligne horizontale commencant a
 # la position (x,y).
 def palette(x, y, couleurs):
 
-    # validite de la palette
-    assert palette_valide(couleurs)# abstraite pour repetition
+    palette_valide(couleurs)
 
-    assert isinstance(x, int)
-    # ecran existant?
-    assert getScreenWidth() > 0
-    # x dans l'ecran?
-    assert x >= 0 and x < getScreenWidth()
+    assert isinstance(x, int)               , "type invalide"
+    assert getScreenWidth() > 0             , "ecran de largeur 0"
+    assert x >= 0 and x < getScreenWidth()  , "coordonee x hors champ"
+    assert len(couleurs)<=getScreenWidth()-x, "palette trop large"
 
-    assert isinstance(y, int)
-    # y dans l'ecran?
-    assert y >= 0 and y < getScreenHeight()
-    # ecran assez large?
-    assert len(couleurs) <= getScreenWidth() - x
+    assert isinstance(y, int)               , "type invalide"
+    assert y >= 0 and y < getScreenHeight() , "coordonee y hors champ"
 
 def palette_valide(couleurs):
-    assert isinstance(couleurs, list)
-    # validite des couleurs
+    assert isinstance(couleurs, list)       , "type invalide"
     for i in list:
-        assert rgb444_valide(i)
+        # validite des couleurs
+        rgb444_valide(i)
 
 def rgb444_valide(couleur):
     # Selon la specification, on accepte le format "#000" a "#FFF"
     # Les exemples montrent aussi des minuscules acceptees
-    assert isinstance(couleur, str)
-    assert len(couleur) == 4
-    assert couleur[0] == "#"
+    assert isinstance(couleur, str)         , "type invalide"
+    assert len(couleur) == 4                , "code couleur de forme invalide"
+    assert couleur[0] == "#"                , "symbole \"#\" manquant"
     for i in range(1, 4):
-        assert i.upper in "0123456789ABCDEF"
+        assert i.upper in "0123456789ABCDEF", "symbole non-hex dans une couleur"
 
-# La procédure afficher prend en entrée deux entiers x et y, une forme (une 
+# La procedure afficher prend en entree deux entiers x et y, une forme (une 
 # matrice 2D contenant des index vers une couleur de la palette de couleur) et
 # une palette de couleurs (une liste de couleurs au format RGB444). Elle affiche
-# la forme à l'écran en utilisant les couleurs correspondantes. La valeur -1
-# dans la forme correspond à une case vide pour laquelle la couleur déjà
-# présente dans le dessin doit être conservée.
+# la forme a l'ecran en utilisant les couleurs correspondantes. La valeur -1
+# dans la forme correspond a une case vide pour laquelle la couleur deja
+# presente dans le dessin doit etre conservee.
 def afficher(x, y, forme, couleurs):
     #TODO: verifier le comportement attendu quand la forme ne rentre pas
 
-    # validite de la palette
     palette_valide(couleurs)
-    # validite de la forme
-    assert forme_valide(forme)
+    forme_valide(forme)
+
     # validite des references forme -> palette
     for rangee in forme:
         for i in rangee:
-            assert i >= -1 and i < len(couleurs)
+            assert i >= -1 and i < len(couleurs), (
+                "forme contient une couleur non-definie"
+            )
 
-    assert isinstance(x, int)
-    # x dans l'ecran
-    assert 0 <= x < getScreenWidth() 
+    assert isinstance(x, int)               , "type invalide"
+    assert 0 <= x < getScreenWidth()        , "coordonee x hors champ"
     # forme dans l'ecran (horizontalement) ssi forme passe deja forme_valide()
-    assert getScreenWidth() >= len(forme[0]) + x
+    assert getScreenWidth()>=len(forme[0])+x, "forme depasse l'ecran"
 
-    assert isinstance(y, int)
-    # y dans l'ecran
-    assert 0 <= y < getScreenHeight()
-    # forme dans l'ecran (verticalement)
-    assert getScreenHeight() >= len(forme) + y
+    assert isinstance(y, int)               , "type invalide"
+    assert 0 <= y < getScreenHeight()       , "coordonee y hors champ"
+    assert getScreenHeight()>=len(forme)+y  , "forme depasse l'ecran"
 
 def forme_valide(forme):
 
-    assert isinstance(forme, list)
+    assert isinstance(forme, list)          , "type invalide"
     largeur = len(forme[0])
     for rangee in forme:
-        assert isinstance(rangee, list)
+        assert isinstance(rangee, list)     , "type invalide"
 
-        # matrice valide?
-        assert len(rangee) == largeur
+        assert len(rangee) == largeur       , "matrice non-rectangulaire"
 
         for i in rangee:
-            assert isinstance(i, int)
+            assert isinstance(i, int)       , "type invalide"
 
             # couleurs dans [-1, +infini[
-            assert i >= -1
+            assert i >= -1                  , "couleur negative indefinissable"
 
-# La procédure effacer prend en entrée quatres entiers. Les deux premiers
-# paramètres, x et y représentent la position du coin supérieur gauche d'un
-# rectangle. Les dimensions du rectangle sont données par les deux derniers
-# paramètres, largeur et hauteur. La procédure doit remplacer tous les pixels
+# La procedure effacer prend en entree quatres entiers. Les deux premiers
+# parametres, x et y représentent la position du coin superieur gauche d'un
+# rectangle. Les dimensions du rectangle sont donnees par les deux derniers
+# parametres, largeur et hauteur. La procedure doit remplacer tous les pixels
 # du rectangle par des pixels noirs ("#000").
 def effacer(x, y, largeur, hauteur)
-#TODO: verifier le comportement attendu quand le rectangle ne rentre pas
+    #TODO: verifier le comportement attendu quand le rectangle ne rentre pas
 
-    assert isinstance(x, int)
-    # x dans l'ecran
-    0 <= x < getScreenWidth()
+    assert isinstance(x, int)               , "type invalide"
+    assert 0 <= x and x < getScreenWidth()  , "coordonee x hors champ"
 
-    assert isinstance(largeur, int)
-    # largeur non-negative
-    largeur >= 0
-    # largeur dans l'ecran
-    getScreenWidth() >= x + largeur 
+    assert isinstance(largeur, int)         , "type invalide"
+    assert largeur >= 0                     , "largeur negative"
+    assert getScreenWidth() >= x + largeur  , "aire definie depasse l'ecran"
 
-    assert isinstance(y, int)
-    # y dans l'ecran
-    0 <= y < getScreenHeight()
+    assert isinstance(y, int)               , "type invalide"
+    assert 0 <= y and y < getScreenHeight() , "coordonee y hors champ"
 
-    assert isinstance(hauteur, int)
-    # hauteur non-negative
-    largeur >= 0
-    # hauteur dans l'ecran
-    getScreenHeight() >= y + hauteur 
+    assert isinstance(hauteur, int)         , "type invalide"
+    assert hauteur >= 0                     , "hauteur negative"
+    assert getScreenHeight() >= y + hauteur , "aire definie depasse l'ecran"
 
 
 # La fonction superpose prend deux formes (des matrices 2D) et retourne une
-# nouvelle forme (une autre matrice 2D) ayant les mêmes dimensions de la forme1,
-# mais à laquelle la forme2 a été ajoutée à la position (x, y). La forme2 est
-# superposée à la forme1 de telle sorte que les pixels de la forme2 remplacent
-# ceux de la forme1 à la position correspondante. Si un pixel de la forme2 est
-# égal à -1, le pixel de la forme1 est conservé.
+# nouvelle forme (une autre matrice 2D) ayant les memes dimensions de la forme1,
+# mais a laquelle la forme2 a ete ajoutee a la position (x, y). La forme2 est
+# superposee a la forme1 de telle sorte que les pixels de la forme2 remplacent
+# ceux de la forme1 a la position correspondante. Si un pixel de la forme2 est
+# egal a -1, le pixel de la forme1 est conserve.
 
 # types:
 # forme1 :: [[int]]
@@ -130,7 +116,9 @@ def effacer(x, y, largeur, hauteur)
 # -1 est la couleur transparente
 
 
-#  La fonction negatif prend une forme en entrée et retourne une nouvelle forme où les pixels colorés sont remplacés par des pixels vides (représentés par -1), et les pixels vides sont remplacés par la couleur 0.
+# La fonction negatif prend une forme en entree et retourne une nouvelle forme
+# ou les pixels colores sont remplaces par des pixels vides (representes par
+# -1), et les pixels vides sont remplaces par la couleur 0.
 
 # types:
 # forme :: [[int]]
@@ -140,9 +128,13 @@ def effacer(x, y, largeur, hauteur)
 # devrait seulement briser sur une matrice heterogene (erreur de type)
 
 
-#  La fonction aleatoire prend quatres entiers en paramètres et retourne une forme de dimensions largeur x hauteur où chaque pixel est assigné à une couleur aléatoire parmi nb_couleurs. Les couleurs sont représentées par des entiers allant de 0 à nb_couleurs - 1.
+# La fonction aleatoire prend quatres entiers en parametres et retourne une
+# forme de dimensions largeur x hauteur ou chaque pixel est assigne a une
+# couleur aleatoire parmi nb_couleurs. Les couleurs sont representees par des
+# entiers allant de 0 a nb_couleurs - 1.
 
-# TODO: demander au prof des explications sur le quatrieme argument (trois entiers definis par la spec)
+# TODO: demander au prof des explications sur le quatrieme argument (trois
+# entiers definis par la spec)
 
 # types:
 # largeur :: int
@@ -152,10 +144,15 @@ def effacer(x, y, largeur, hauteur)
 # assertions:
 # largeur >= 0
 # hauteur >= 0
-# nb_couleurs > 0 # selon la specification, aleatoire ne contient jamais la couleur -1
+# nb_couleurs > 0 # selon la spec, aleatoire ne contient jamais la couleur -1
 
 
-#  La fonction incrementer_couleurs prend une forme et un nombre de couleurs en paramètres, et retourne une nouvelle forme de mêmes dimensions où chaque pixel coloré est remplacé par la couleur suivante dans une palette de couleurs allant de 0 à nb_couleurs - 1. Si un pixel a la couleur nb_couleurs - 1, il est remplacé par la couleur 0. Les pixels vides (représentés par -1) restent inchangés.
+# La fonction incrementer_couleurs prend une forme et un nombre de couleurs en
+# parametres, et retourne une nouvelle forme de memes dimensions ou chaque pixel
+# colore est remplace par la couleur suivante dans une palette de couleurs
+# allant de 0 a nb_couleurs - 1. Si un pixel a la couleur nb_couleurs - 1, il
+# est remplace par la couleur 0. Les pixels vides (representes par -1) restent
+# inchanges.
 
 # types:
 # forme :: [[int]]
@@ -166,26 +163,35 @@ def effacer(x, y, largeur, hauteur)
 # forme ne contient que c t.q. nb_couleurs > c
 
 # notes:
-# operation pixel-par-pixel seulement; forme n'a pas besoin de dimensions valides
+# operation pixel-par-pixel; forme n'a pas besoin de dimensions valides
 # utiliser % (mod) pour rester dans le domaine de nb_couleurs
 # on ignore les pixels de couleur -1
 
 
-#  La fonction decrementer_couleurs a le comportement inverse de incrementer_couleurs. Elle prend une forme et un nombre de couleurs en paramètres, et retourne une nouvelle forme de mêmes dimensions où chaque pixel coloré est remplacé par la couleur précédente dans la palette de couleurs. Si un pixel a la couleur 0, il est remplacé par la couleur nb_couleurs - 1. Les pixels vides (représentés par -1) restent inchangés.
+# La fonction decrementer_couleurs a le comportement inverse de
+# incrementer_couleurs. Elle prend une forme et un nombre de couleurs en
+# parametres, et retourne une nouvelle forme de memes dimensions ou chaque pixel
+# colore est remplace par la couleur precedente dans la palette de couleurs. Si
+# un pixel a la couleur 0, il est remplace par la couleur nb_couleurs - 1. Les
+# pixels vides (representes par -1) restent inchanges.
 
 # voir incrementer_couleurs
 # rotation gauche sur palette
 
 
-#  La fonction rotation_horaire prend une forme en entrée et retourne une nouvelle forme qui est la rotation de la forme d'entrée dans le sens horaire.
+# La fonction rotation_horaire prend une forme en entree et retourne une
+# nouvelle forme qui est la rotation de la forme d'entree dans le sens horaire.
 
 # types:
 # forme :: [[int]]
 
-# notes: on peut utiliser la multiplication matricielle ssi forme est une matrice valide
+# notes: on peut utiliser la multiplication matricielle ssi forme est une
+# matrice valide
 
 
-#  La fonction rotation_antihoraire prend une forme en entrée et retourne une nouvelle forme qui est la rotation de la forme d'entrée dans le sens antihoraire.
+# La fonction rotation_antihoraire prend une forme en entree et retourne une
+# nouvelle forme qui est la rotation de la forme d'entree dans le sens
+# antihoraire.
 
 # voir rotation_horaire
 
--- tp.py
+++ tp.py
@@ -1,20 +1,21 @@
 import tests
 
 def palette(x, y, couleurs):
-    assert tests.palette(x, y, couleurs)
+    tests.palette(x, y, couleurs)# assertions
     for i,c in enumerate(couleurs):
         setPixel(x+i, y, c)
 
 def afficher(x, y, forme, couleurs):
+    tests.afficher(x, y, forme, couleurs)# assertions
     #TODO: verifier si les axes sont bien definis
     # voir la convention des axes; y, j := rangees
-    for j in range(len(forme)):
-        # x, i := colonnes
-        for i,c in enumerate(forme[j]):
-            if c != -1:# si non-transparent
-                setPixel(x+i,y+j,couleurs[c])
+    for j, rangee in enumerate(forme):
+        for i, couleur in enumerate(rangee):
+            if couleur != -1:# si non-transparent
+                setPixel(x+i, y+j, couleurs[couleur])
 
 def effacer(x, y, largeur, hauteur):
+    tests.effacer(x, y, largeur, hauteur)# assertions
     fillRectangle(x, y, largeur, hauteur, "#000")
 
 def superpose(forme1, forme2, x, y):
