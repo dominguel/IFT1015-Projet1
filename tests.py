cf3cdf0a4fa420c97a79054fabb9e3fbd65f6e9b
dominguel <lucas.domingue@gmail.com>
2026-02-18 14:52:04 -0500

    3/11 terminés

--- tests.py
+++ tests.py
@@ -1,51 +1,120 @@
-#  La procédure palette prend en paramètre deux entiers x et y et une liste de couleurs en format RGB444. Elle affiche cette palette de couleur sur une ligne horizontale commencant à la position (x,y).
+# La procédure palette prend en paramètre deux entiers x et y et une liste de
+# couleurs en format RGB444.
+# Elle affiche cette palette de couleur sur une ligne horizontale commencant à
+# la position (x,y).
+def palette(x, y, couleurs):
 
-# types:
-# x :: int
-# y :: int
-# couleurs :: [string]
+    # validite de la palette
+    assert palette_valide(couleurs)# abstraite pour repetition
 
-# assertions:
-# 0 <= x < getScreenWidth() # x dans l'ecran
-# 0 <= y < getScreenHeight() # y dans l'ecran
-# couleurs ne contient que des strings valides RGB444
-# getScreenWidth() >= len(couleurs) + x # palette rentre dans l'ecran
+    assert isinstance(x, int)
+    # ecran existant?
+    assert getScreenWidth() > 0
+    # x dans l'ecran?
+    assert x >= 0 and x < getScreenWidth()
+
+    assert isinstance(y, int)
+    # y dans l'ecran?
+    assert y >= 0 and y < getScreenHeight()
+    # ecran assez large?
+    assert len(couleurs) <= getScreenWidth() - x
+
+def palette_valide(couleurs):
+    assert isinstance(couleurs, list)
+    # validite des couleurs
+    for i in list:
+        assert rgb444_valide(i)
+
+def rgb444_valide(couleur):
+    # Selon la specification, on accepte le format "#000" a "#FFF"
+    # Les exemples montrent aussi des minuscules acceptees
+    assert isinstance(couleur, str)
+    assert len(couleur) == 4
+    assert couleur[0] == "#"
+    for i in range(1, 4):
+        assert i.upper in "0123456789ABCDEF"
+
+# La procédure afficher prend en entrée deux entiers x et y, une forme (une 
+# matrice 2D contenant des index vers une couleur de la palette de couleur) et
+# une palette de couleurs (une liste de couleurs au format RGB444). Elle affiche
+# la forme à l'écran en utilisant les couleurs correspondantes. La valeur -1
+# dans la forme correspond à une case vide pour laquelle la couleur déjà
+# présente dans le dessin doit être conservée.
+def afficher(x, y, forme, couleurs):
+    #TODO: verifier le comportement attendu quand la forme ne rentre pas
+
+    # validite de la palette
+    palette_valide(couleurs)
+    # validite de la forme
+    assert forme_valide(forme)
+    # validite des references forme -> palette
+    for rangee in forme:
+        for i in rangee:
+            assert i >= -1 and i < len(couleurs)
+
+    assert isinstance(x, int)
+    # x dans l'ecran
+    assert 0 <= x < getScreenWidth() 
+    # forme dans l'ecran (horizontalement) ssi forme passe deja forme_valide()
+    assert getScreenWidth() >= len(forme[0]) + x
+
+    assert isinstance(y, int)
+    # y dans l'ecran
+    assert 0 <= y < getScreenHeight()
+    # forme dans l'ecran (verticalement)
+    assert getScreenHeight() >= len(forme) + y
+
+def forme_valide(forme):
+
+    assert isinstance(forme, list)
+    largeur = len(forme[0])
+    for rangee in forme:
+        assert isinstance(rangee, list)
+
+        # matrice valide?
+        assert len(rangee) == largeur
+
+        for i in rangee:
+            assert isinstance(i, int)
+
+            # couleurs dans [-1, +infini[
+            assert i >= -1
+
+# La procédure effacer prend en entrée quatres entiers. Les deux premiers
+# paramètres, x et y représentent la position du coin supérieur gauche d'un
+# rectangle. Les dimensions du rectangle sont données par les deux derniers
+# paramètres, largeur et hauteur. La procédure doit remplacer tous les pixels
+# du rectangle par des pixels noirs ("#000").
+def effacer(x, y, largeur, hauteur)
+#TODO: verifier le comportement attendu quand le rectangle ne rentre pas
+
+    assert isinstance(x, int)
+    # x dans l'ecran
+    0 <= x < getScreenWidth()
+
+    assert isinstance(largeur, int)
+    # largeur non-negative
+    largeur >= 0
+    # largeur dans l'ecran
+    getScreenWidth() >= x + largeur 
+
+    assert isinstance(y, int)
+    # y dans l'ecran
+    0 <= y < getScreenHeight()
+
+    assert isinstance(hauteur, int)
+    # hauteur non-negative
+    largeur >= 0
+    # hauteur dans l'ecran
+    getScreenHeight() >= y + hauteur 
 
 
-#  La procédure afficher prend en entrée deux entiers x et y, une forme (une matrice 2D contenant des index vers une couleur de la palette de couleur) et une palette de couleurs (une liste de couleurs au format RGB444). Elle affiche la forme à l'écran en utilisant les couleurs correspondantes. La valeur -1 dans la forme correspond à une case vide pour laquelle la couleur déjà présente dans le dessin doit être conservée.
-
-# types:
-# x :: int
-# y :: int
-# forme :: [[int]]
-# couleurs :: [string]
-
-# assertions:
-# 0 <= x < getScreenWidth() # x dans l'ecran
-# 0 <= y < getScreenHeight() # y dans l'ecran
-# couleurs ne contient que des strings valides RGB444
-# forme est une matrice valide
-# getScreenWidth() >= len(forme[0]) + x # forme dans l'ecran, ssi forme est une matrice valide
-# getScreenHeight() >= len(forme) + y # forme dans l'ecran (verticalement)
-# forme ne contient que c t.q. -1 <= c < len(couleurs)
-
-
-#  La procédure effacer prend en entrée quatres entiers. Les deux premiers paramètres, x et y représentent la position du coin supérieur gauche d'un rectangle. Les dimensions du rectangle sont données par les deux derniers paramètres, largeur et hauteur. La procédure doit remplacer tous les pixels du rectangle par des pixels noirs ("#000").
-
-# types:
-# x :: int
-# y :: int
-# largeur :: int
-# hauteur :: int
-
-# assertions:
-# 0 <= x < getScreenWidth() # x dans l'ecran
-# 0 <= y < getScreenHeight() # y dans l'ecran
-# getScreenWidth() >= x + largeur # rectangle dans l'ecran
-# getScreenHeight() >= y + hauteur # rectangle dans l'ecran
-
-
-#  La fonction superpose prend deux formes (des matrices 2D) et retourne une nouvelle forme (une autre matrice 2D) ayant les mêmes dimensions de la forme1, mais à laquelle la forme2 a été ajoutée à la position (x, y). La forme2 est superposée à la forme1 de telle sorte que les pixels de la forme2 remplacent ceux de la forme1 à la position correspondante. Si un pixel de la forme2 est égal à -1, le pixel de la forme1 est conservé.
+# La fonction superpose prend deux formes (des matrices 2D) et retourne une
+# nouvelle forme (une autre matrice 2D) ayant les mêmes dimensions de la forme1,
+# mais à laquelle la forme2 a été ajoutée à la position (x, y). La forme2 est
+# superposée à la forme1 de telle sorte que les pixels de la forme2 remplacent
+# ceux de la forme1 à la position correspondante. Si un pixel de la forme2 est
+# égal à -1, le pixel de la forme1 est conservé.
 
 # types:
 # forme1 :: [[int]]
--- tp.py
+++ tp.py
@@ -1,31 +1,11 @@
+import tests
+
 def palette(x, y, couleurs):
-    pass# undefined
-    # impure function; outputs to display
-
-    # proper type signature
-    assert type(x) is int
-    assert type(y) is int
-#    assert type(couleurs) is #TODO: liste de couleurs
-
-    # tests potentiels
-    assert len(couleurs) <= getScreenWidth()-x
-
+    assert tests.palette(x, y, couleurs)
     for i,c in enumerate(couleurs):
         setPixel(x+i, y, c)
 
 def afficher(x, y, forme, couleurs):
-    pass# undefined
-
-    # proper type signature
-    assert type(x) is int
-    assert type(y) is int
-#    assert type(couleurs) is #TODO: liste de couleurs
-#    TODO: assert forme est une matrice valide
-
-    # tests potentiels
-    # forme contient des index -1 <= i < len(couleurs)
-    # forme a des dimensions valides selon getScreenWidth(), getScreenHeight()
-
     #TODO: verifier si les axes sont bien definis
     # voir la convention des axes; y, j := rangees
     for j in range(len(forme)):
@@ -35,10 +15,6 @@ def afficher(x, y, forme, couleurs):
                 setPixel(x+i,y+j,couleurs[c])
 
 def effacer(x, y, largeur, hauteur):
-    pass# undefined
-
-    #TODO: type signature, tests, etc...
-
     fillRectangle(x, y, largeur, hauteur, "#000")
 
 def superpose(forme1, forme2, x, y):
